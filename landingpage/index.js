021cc2edfd6024a6e8af07c4cf845612cc402624
Divya <enjamuridivya@gmail.com>
2019-08-05 19:41:45 -0700

    landing page index.js

---
+++ landingpage/index.js
@@ -0,0 +1,13 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCcqYafYLVS5QvocHANk0illfsIYUqH3I4",
+    authDomain: "cart-to-kitchen-4c135.firebaseapp.com",
+    databaseURL: "https://cart-to-kitchen-4c135.firebaseio.com",
+    projectId: "cart-to-kitchen-4c135",
+    storageBucket: "cart-to-kitchen-4c135.appspot.com",
+    messagingSenderId: "421625026826",
+    appId: "1:421625026826:web:988bc65eda645a60"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
